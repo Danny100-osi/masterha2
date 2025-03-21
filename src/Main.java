ce3f7088abb45c09e34a412ccee51d4a25457304
Danyp <dannyperezfyac5@gmail.com>
2025-03-20 21:30:24 -0600

    LISTAS DE INVENTARIO

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> nombresProductos = new ArrayList<>();
+        ArrayList<String> cantidades = new ArrayList<>();
+        ArrayList<String> precios = new ArrayList<>();;
+
+
+        System.out.println("ingrese producto:");
+        ingreseDatos = (scanner.next());
+
+
+
+    }
+}
